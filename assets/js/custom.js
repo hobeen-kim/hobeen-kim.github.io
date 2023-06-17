c0dbd20b3c9ca086a04f0f10245fef14215ca4a2
kimhobeen <sksjsksh32@naver.com>
2023-06-17 17:26:53 +0900

    update custom

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
     $(".collapsible").click(function() {
+
+        var $content = $('.series-ol');
+        // check if max-height is 0 (closed)
+        if ($content.css('max-height') === '0px'){
+            // change max-height to the scrollHeight
+            $content.css('max-height', $content.prop('scrollHeight') + 'px');
+        } else {
+            $content.css('max-height', '0');
+        }
+        
         var seriesList = document.querySelector('.series-ol');
         $(this).next().toggle(); // 시리즈 목록 표시/숨김
         if ($(this).text() == "목록보기") {

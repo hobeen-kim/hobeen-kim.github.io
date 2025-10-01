2e54c30fa7df6a4ab78c7b3c5246e98e67cc1687
hobeen-kim <sksjsksh32@gmail.com>
2025-10-01 18:00:23 +0900

    update

--- docs/.vuepress/theme/SidebarCategory.vue
+++ docs/.vuepress/theme/SidebarCategory.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="custom-sidebar">
     <div class="hovering">
-      <p class="vp-sidebar-item vp-sidebar-heading hovering">글</p>
+      <p class="custom-sidebar-item custom-sidebar-heading">글</p>
+      <ul>
+        <li><a class="custom-sidebar-item vp-sidebar-children" :href="`/posts/`">모두</a></li>
+        <li v-for="category in categories" :key="category">
+          <a class="custom-sidebar-item custom-sidebar-children" :href="`/posts/${category.name}/`">{{ toKorean(category.name) }}</a>
+        </li>
+      </ul>
     </div>
-    <ul>
-      <li><a class="vp-sidebar-item" :href="`/posts/`">모두</a></li>
-      <li v-for="category in categories" :key="category">
-        <a class="vp-sidebar-item" :href="`/posts/${category.name}/`">{{ toKorean(category.name) }}</a>
-      </li>
-    </ul>
-    <div class="hovering" style="margin-top: 5px">
-      <a class="vp-sidebar-item vp-sidebar-heading " href="/books/">책</a>
+    <div class="custom-sidebar-heading hovering" style="margin-top: 5px">
+      <a class="custom-sidebar-item" href="/books/">책</a>
     </div>
-    <div class="hovering" style="margin-top: 10px">
-      <a class="vp-sidebar-item vp-sidebar-heading" href="/logs/">짧은 글</a>
+    <div class="custom-sidebar-heading  hovering" style="margin-top: 10px">
+      <a class="custom-sidebar-item" href="/logs/">짧은 글</a>
+    </div>
+    <div class="custom-sidebar-heading  hovering" style="margin-top: 10px">
+      <a class="custom-sidebar-item" href="/resume/">이력서</a>
     </div>
   </div>
 </template>
@@ -42,14 +45,28 @@ export default {
 .custom-sidebar {
   margin-left: 15px;
   margin-right: 15px;
+  margin-bottom: 70px;
   padding-top: 15px;
   border-top: 1px solid var(--vp-c-border);
 }
 
+.custom-sidebar-item {
+  padding: 0.3rem 1.5rem 0.3rem 1.25rem;
+  color: var(--vp-c-text);
+}
+
+.custom-sidebar-heading {
+  box-sizing: border-box;
+  width: 100%;
+  font-weight: 700;
+  font-size: 1.1em;
+  transition: color 0.15s ease;
+  padding-top: 0.2rem;
+  padding-bottom: 0.2rem;
+}
+
 ul {
   list-style: none;
-  padding-left: 0;
-  margin: 0;
 
   :hover {
     cursor: pointer;
